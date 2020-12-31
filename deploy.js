8879d556f3fe93d9e3c156b632100dd5bb8f2c66
ljz3 <kevin.ottawa@hotmail.com>
2020-12-31 01:45:01 -0500

    Added compile and deploy scripts

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'INSERT MNEMONIC PHRASE HERE',
+    'https://rinkeby.infura.io/v3/0903b6ff89ce48ebacacad2c3e0f0b1b'
+);
+
+const web3 = new Web3(provider);
+
+
+const INIT_STR = 'hello'
+
+const deploy = async () => {
+
+    const accounts = await web3.eth.getAccounts();
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: '0x' + bytecode}) // add 0x bytecode
+        .send({from: accounts[0]}); // remove 'gas'
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
+
